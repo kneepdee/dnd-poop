cb48f0a503e8873d11fb7b4988768fa3a6150dab
Gabriel Kuslap <gabriel.kuslap@gmail.com>
2020-03-13 18:23:42 +0200

    Updates

---
+++ precache-manifest.895f23f2e14e06bb6877c84f7934dfe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec7d79f03488015501b43b2ed69beb2",
+    "url": "/dnd-poop/index.html"
+  },
+  {
+    "revision": "f4137518063d0c42203b",
+    "url": "/dnd-poop/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3514339f1f6fa6dca057",
+    "url": "/dnd-poop/static/js/2.1f9b113e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dnd-poop/static/js/2.1f9b113e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4137518063d0c42203b",
+    "url": "/dnd-poop/static/js/main.470f0dc6.chunk.js"
+  },
+  {
+    "revision": "820bcf95c0cfb76f0fd6",
+    "url": "/dnd-poop/static/js/runtime-main.b3e4c4ab.js"
+  }
+]);

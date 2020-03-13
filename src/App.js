f6c577324111b181730b572b2a21660f694674ed
Gabriel Kuslap <gabriel.kuslap@gmail.com>
2020-03-13 17:33:50 +0200

    Enable touch dragging (no preview)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
 import React, { useState } from 'react';
 import './App.css';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+// import TouchBackend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
 
-function Quarter({ children }) {
-  return <div>{children}</div>;
+const ItemTypes = {
+  STAR: 'star'
+};
+
+function Quarter({ children, moveStar, x, y }) {
+  const [, drop] = useDrop({
+    accept: ItemTypes.STAR,
+    drop: () => moveStar(x, y)
+  });
+
+  const position =
+    x === 0 && y === 0
+      ? 'upperLeft'
+      : x === 1 && y === 0
+      ? 'upperRight'
+      : x === 0 && y === 1
+      ? 'lowerLeft'
+      : x === 1 && x === 1
+      ? 'lowerRight'
+      : null;
+
+  const flexValues = {
+    upperLeft: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start'
+    },
+    upperRight: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-end'
+    },
+    lowerLeft: {
+      alignItems: 'flex-end',
+      justifyContent: 'flex-start'
+    },
+    lowerRight: {
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end'
+    }
+  };
+
+  const style = {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    alignItems: flexValues[position].alignItems,
+    justifyContent: flexValues[position].justifyContent
+  };
+
+  return (
+    <div style={style} ref={drop}>
+      {children}
+    </div>
+  );
 }
 
 function Star({ indexNumber }) {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.STAR },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  });
   return (
     <span
       style={{
-        position: 'absolute',
-        fontSize: '4em'
+        fontSize: '4em',
+        opacity: isDragging ? 0.5 : 1,
+        cursor: 'move'
       }}
       role="img"
       aria-label="star"
+      ref={drag}
     >
       ⭐️{indexNumber}
     </span>
@@ -41,30 +103,39 @@ function renderQuarter(i, starPosition, setStarPosition) {
       onClick={() => moveStar(x, y)}
       key={i}
     >
-      <Quarter>{piece}</Quarter>
+      <Quarter x={x} y={y} moveStar={moveStar}>
+        {piece}
+      </Quarter>
     </div>
   );
 }
 
 function Layout() {
   const [starPosition, setStarPosition] = useState([0, 0]);
+  console.log('starPosition:', starPosition);
 
   const quarters = [];
   for (let i = 0; i < 4; i++) {
     quarters.push(renderQuarter(i, starPosition, setStarPosition));
   }
 
+  const opts = {
+    enableMouseEvents: false
+  };
+
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        flexWrap: 'wrap'
-      }}
-    >
-      {quarters}
-    </div>
+    <DndProvider backend={TouchBackend} options={opts}>
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          flexWrap: 'wrap'
+        }}
+      >
+        {quarters}
+      </div>
+    </DndProvider>
   );
 }
 
